4e9bd192869fb57575b62480a3f269fba213941a
siddharthbaleja7 <144931848+siddharthbaleja7@users.noreply.github.com>
2024-05-26 23:45:00 +0530

    Update end.js

--- end.js
+++ end.js
@@ -1,4 +1,4 @@
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
-finalScore.innerText = `Your Score: ${mostRecentScore}`;
+finalScore.innerText = `Your Score: ${mostRecentScore}`; 
